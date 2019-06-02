f4ed5f7fcfaa4cc00e7aec8d985d6312dc5a9014
be350 <be350@gmail.com>
2019-06-02 23:00:05 +0900

    second commit

--- subproject/subproject/newapp/views.py
+++ subproject/subproject/newapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def main(request): # 요청을 받아와서 main(main이라고 안하고 a라고 해도됨 임의로 정한 것)을 수행하겠다
-    text="글자를 입력하세요."
+    text="글자를 입력하세요"
     context={'text':text}
     return render(request,'newapp/index.html',context) #request에는 누구로부터 받아왔는지 담겨있음
 
